50b81530a4d0b1b53dd355b3d5e945de63f64a6e
wintaraputra242 <wintaraputra242@gmail.com>
2026-09-24 10:11:30 +0800

    fix: fix ignore file or directory

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-06918fc1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hkfvd1o8c1o"
+    "revision": "0.r0gsgsbhsto"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
